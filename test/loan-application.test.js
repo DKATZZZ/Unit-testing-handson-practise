793cbeecc81df9b4d5ada69579c1450f7b2e54f5
malar <malarvizhiravichandran8@gmail.com>
2023-08-29 19:59:15 +0530

    test case update

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -3,5 +3,27 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../loan-application.js';
 
 describe('LoanApplication', () => {
- 
+    it('has a default title "Hey there"', async () => {
+        const el = await fixture(html`<loan-application></loan-application>`);
+        expect(el.title).to.equal('Hey there');
+      });
+    
+      it('can override the title via attribute', async () => {
+        const el = await fixture(html`<loan-application title="different"></loan-application>`);
+        expect(el.title).to.equal('different');
+      });
+    
+      it('increments the counter on button click', async () => {
+        const el = await fixture(html`<loan-application></loan-application>`);
+        el.shadowRoot.querySelector('button').click();
+        expect(el.counter).to.equal(6);
+      });
+    
+      it('passes properties to dashboard component', async () => {
+        const el = await fixture(html`<loan-application></loan-application>`);
+        const dashboard = el.shadowRoot.querySelector('dash-board');
+        expect(dashboard).to.exist;
+        expect(dashboard.title).to.equal(el.title);
+        expect(dashboard.counter).to.equal(el.counter);
+      });
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -3,4 +3,42 @@ import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
 describe('Loan EMI details', () => {
   // Write test cases inside this block
+  it('renders EMI details correctly', async () => {
+    const emiDetails = {
+      interestRate: 5,
+      monthlyEMI: 1000,
+      principal: 5000,
+      interest: 1000,
+      totalAmount: 6000,
+    };
+
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    el._data = emiDetails;
+
+    const detailsParagraphs = el.shadowRoot.querySelectorAll('.emi-details p');
+    expect(detailsParagraphs.length).to.equal(5);
+    expect(detailsParagraphs[0].textContent).to.include('Interest Rate:');
+    expect(detailsParagraphs[0].querySelector('span').textContent).to.equal('5 %');
+
+    const buttons = el.shadowRoot.querySelectorAll('.btn-cont lion-button');
+    expect(buttons.length).to.equal(2);
+  });
+
+  it('navigates to basic details page on cancel button click', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    const cancelButton = el.shadowRoot.querySelector('.cancel-btn');
+    const goSpy = sinon.spy(el, '_toBasicDetails');
+
+    cancelButton.click();
+    expect(goSpy).to.have.been.calledOnce;
+  });
+
+  it('navigates to customer page on continue button click', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    const continueButton = el.shadowRoot.querySelector('.continue-btn');
+    const goSpy = sinon.spy(el, '_toCustomer');
+
+    continueButton.click();
+    expect(goSpy).to.have.been.calledOnce;
+  });
 });
